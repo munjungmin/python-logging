be32329548d4d52377a9fc163a6dac733a51aa17
munjungmin <answjdals77@gmail.com>
2026-04-16 13:21:51 +0900

    Exception 발생 시 로그 출력

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
+from fastapi import FastAPI
 import logging
-import database
-import auth
+import uvicorn
 
 logging.basicConfig(
     level=logging.INFO,
-    format="%(name)s %(asctime)s - %(levelname)s - %(message)s",
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler('app.log'),  # To file
+        logging.StreamHandler()  # To terminal
+    ]
 )
 
-logger = logging.getLogger(__name__) # child logger를 생성 (logger의 이름이 모듈 경로)
+logger = logging.getLogger(__name__)
 
-logger.info("Application started")
-database.connect()
-database.query_users()
-auth.login("alice")
-auth.logout("alice")
-logger.info("Application finished")
+app = FastAPI()
+
+@app.get("/divide")
+def divide(a: int, b: int):
+    try:
+        result = a / b
+        return {"result": result}
+    except Exception as e:
+        logger.error("Division failed", exc_info=True) # trace stack을 볼 수 있음
+        # logger.exception("Division failed")          
+        return {"error": "Something went wrong"}
+
+# Run the server when script is executed
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
